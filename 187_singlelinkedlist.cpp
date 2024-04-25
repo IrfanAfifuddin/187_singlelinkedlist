bc534145fc782e6e8d6e3dc0947a4c7a669bca0e
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2024-04-25 10:44:38 +0700

    membuat void searchData

--- 187_singlelinkedlist.cpp
+++ 187_singlelinkedlist.cpp
@@ -102,3 +102,10 @@ void traverse() {
     }
 }
 
+void searchData() {
+    if (listEmpty()) {
+        cout << "List Kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
